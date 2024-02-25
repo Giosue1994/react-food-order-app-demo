be2ac79a80583c0092c24c5b85ebc13b043129f1
Giosuè Lentini <63297733+Giosue1994@users.noreply.github.com>
2024-02-25 11:34:09 +0100

    modale carrello

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,51 @@
 import logo from "../assets/logo.jpg";
 import Button from "./Button";
-import { useContext } from "react";
+import { useContext, useRef } from "react";
 import { CartContext } from "../store/cart-context";
+import Modal from "./Modal.jsx";
+import Cart from "./Cart.jsx";
 
 export default function Header() {
   const { items } = useContext(CartContext);
+  const modal = useRef();
 
   const totalQuantity = items.reduce((totalNumberOfItems, item) => {
     return totalNumberOfItems + item.quantity;
   }, 0);
 
-  return (
-    <header id="main-header">
-      <div id="title">
-        <img src={logo} alt="burger" />
-        <h1>reactfood</h1>
-      </div>
+  function handleOpenCart() {
+    modal.current.open();
+  }
 
-      <nav>
-        <Button buttonType={"text"}>Cart({totalQuantity})</Button>
-      </nav>
-    </header>
+  let modalActions = <Button buttonType="text">Chiudi</Button>;
+
+  if (totalQuantity > 0) {
+    modalActions = (
+      <>
+        <Button buttonType="text">Chiudi</Button>
+        <Button buttonType="button">Ceckout</Button>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <Modal title="Il tuo carrello" actions={modalActions} ref={modal}>
+        <Cart />
+      </Modal>
+
+      <header id="main-header">
+        <div id="title">
+          <img src={logo} alt="burger" />
+          <h1>reactfood</h1>
+        </div>
+
+        <nav>
+          <Button buttonType={"text"} onClick={handleOpenCart}>
+            Cart({totalQuantity})
+          </Button>
+        </nav>
+      </header>
+    </>
   );
 }
